30713d6486ef58493e4ba84a61d6512a90b654bc
z008hy <z008hy@163.com>
2019-06-03 22:09:08 +0800

    updata to webpack4

--- simple-webpack/webpack.config.js
+++ simple-webpack/webpack.config.js
@@ -1,87 +1,37 @@
-const path = require('path');
-const webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const autoprefixer = require('autoprefixer')
-
-const postcssLoader = {
-  loader: 'postcss-loader',
-  options: {
-    plugins: () => [autoprefixer]
-  }
-}
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
+  mode: 'development',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.js',
-    libraryTarget: 'umd'
+    filename: '[name].js',
+    publicPath: '/'
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true
-        }
-      },
-      {
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        enforce: 'pre',
-        exclude: /node_modules/,
-        options: {
-          formatter: require('eslint-friendly-formatter')
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract(
-          {
-            fallback: 'style-loader',
-            use: ['css-loader?minimize=true', postcssLoader]
-          })
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract(
-          {
-            fallback: 'style-loader',
-            use: ['css-loader?minimize=true', postcssLoader, 'sass-loader']
-          })
-      },
-      {
-        test: /\.(jpg|png|gif|ico)(\?.*)?(#.*)?$/,
-        loader: 'url-loader',
-        query: {
-          name: '[name].[ext]',
-          limit: 4096
-        }
-      },
-      {
-        test: /\.(svg|eot|ttf|woff|woff2)(\?.*)?(#.*)?$/,
-        loader: 'file-loader',
-        query: {
-          // name: env.LOCAL ? '[name].[ext]' : '[name].[hash].[ext]'
-          name: '[name].[ext]'
-        }
+        loader: 'babel-loader'
       }
     ]
   },
   plugins: [
-    // new webpack.optimize.UglifyJsPlugin({
-    //   beautify: false,
-    //   comments: false,
-    //   sourceMap: false,
-    //   compress: {
-    //     warnings: false,
-    //     drop_console: true,
-    //     collapse_vars: true,
-    //     reduce_vars: true
-    //   }
-    // }),
-    new ExtractTextPlugin('index.css')
-  ]
-};
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html',
+      inject: true
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist/'),
+    clientLogLevel: 'warning',
+    disableHostCheck: true,
+    port: 8000,
+    hot: true,
+    contentBase: false,
+    compress: true,
+    quiet: true
+  }
+}
